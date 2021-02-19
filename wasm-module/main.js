8e8c78c102622603bd67a21ac137ebd7945ab919
Sosthene00 <674694@protonmail.ch>
2021-02-19 15:36:07 +0100

    Readibility improvements on main.js
    - add a label to convertToString to allow for clearer message error
    - convertToString everywhere
    - console.error when error

--- wasm-module/main.js
+++ wasm-module/main.js
@@ -20,12 +20,21 @@ function toHexString(byteArray) {
   }).join('')
 }
 
-function convertToString(ptr) {
+function convertToString(ptr, label) {
+  if (ptr === 0) {
+    console.error("convertToString was given a NULL pointer for " + label);
+    return "";
+  }
+
   let str = UTF8ToString(ptr);
 
   if (ccall('wally_free_string', 'number', ['number'], [ptr]) !== 0) {
     console.error("ptr to " + str + " wasn't freed");
     return "";
+  }
+
+  if (str === "") {
+    console.error("convertToString was given a pointer to an empty string for " + label);
   }
 
   return str;
@@ -50,7 +59,7 @@ function init() {
   let is_elements = ccall('is_elements', 'number', [], [])
 
   if (is_elements !== 1) {
-    console.log("libwally is not build with elements");
+    console.error("libwally is not build with elements");
     return -1;
   }
 
@@ -107,34 +116,29 @@ function generateWallet(userPassword, mnemonic) {
 
   let passphrase = PASSPHRASE; // for now we can define passphrase as a constant, e.g. the application name.
 
-/*   I don't think it's useful to leave the user choose a passphrase along with his seed, 
-  I see it as an error prone and unecessary feature for our use case, user already have a userPassword to remember.
-  It makes sense to define it as constant literal string to prevent collision between different applications.
-  But this behaviour can easily be changed if necessary. */
+  /** I don't think it's useful to leave the user choose a passphrase along with his seed, 
+  *   I see it as an error prone and unecessary feature for our use case, user already have a userPassword to remember.
+  *   It makes sense to define it as constant literal string to prevent collision between different applications.
+  *   But this behaviour can easily be changed if necessary. 
+  **/
 
   // generate the seed from the mnemonic
   if ((seed_ptr = ccall('generateSeed', 'number', ['string', 'string'], [mnemonic, passphrase])) === "") {
-    console.log("generateMnemonic failed");
+    console.error("generateMnemonic failed");
     return "";
   }
 
-  let seed_hex = UTF8ToString(seed_ptr);
-
-  if (ccall('wally_free_string', 'number', ['number'], [seed_ptr]) !== 0) {
-    console.log("seed_ptr wasn't freed");
+  if ((seed = convertToString(seed_ptr, "seed")) === "") {
     return "";
   }
 
   // generate a master key and serialize extended keys to base58
-  if ((xprv_ptr = ccall('hdKeyFromSeed', 'number', ['string'], [seed_hex])) === "") {
-    console.log("hdKeyFromSeed failed");
+  if ((xprv_ptr = ccall('hdKeyFromSeed', 'number', ['string'], [seed])) === "") {
+    console.error("hdKeyFromSeed failed");
     return "";
   }
 
-  let xprv = UTF8ToString(xprv_ptr);
-
-  if (ccall('wally_free_string', 'number', ['number'], [xprv_ptr]) !== 0) {
-    console.log("xprv_ptr wasn't freed");
+  if ((xprv = convertToString(xprv_ptr, "xprv")) === "") {
     return "";
   }
 
@@ -184,17 +188,14 @@ function newWallet(userPassword) {
 
   // generate a mnemonic (seed words) from this entropy
   if ((mnemonic_ptr = ccall('generateMnemonic', 'number', ['array', 'number'], [entropy, entropy.length])) === "") {
-    console.log("generateMnemonic failed");
+    console.error("generateMnemonic failed");
     return "";
   }
 
   // Overwrite the entropy used since we don't need it anymore
   window.crypto.getRandomValues(entropy);
 
-  let mnemonic = UTF8ToString(mnemonic_ptr);
-
-  if (ccall('wally_free_string', 'number', ['number'], [mnemonic_ptr]) !== 0) {
-    console.log("mnemonic wasn't freed");
+  if ((mnemonic = convertToString(mnemonic_ptr, "mnemonic")) === "") {
     return "";
   }
 
@@ -207,21 +208,18 @@ function restoreWallet(userPassword, mnemonic) {
 
 function decryptWallet(encryptedWallet, userPassword) {
   if ((clearWallet_ptr = ccall('decryptFileWithPassword', 'number', ['string', 'string'], [encryptedWallet, userPassword])) === "") {
-    console.log("decryptFileWithPassword failed");
+    console.error("decryptFileWithPassword failed");
     return "";
   };
 
-  let clearWallet = UTF8ToString(clearWallet_ptr);
-
-  if (ccall('wally_free_string', 'number', ['number'], [clearWallet_ptr]) !== 0) {
-    console.log("mnemonic wasn't freed");
+  if ((clearWallet = convertToString(clearWallet_ptr, "clearWallet")) === "") {
     return "";
   }
 
   try {
     JSON.parse(clearWallet);
   } catch(e) {
-    console.log("Can't decrypt the wallet.");
+    console.error("Can't decrypt the wallet.");
     if (e instanceof SyntaxError) {
       alert("Failed to decrypt your wallet, check your password and try again.");
     }
@@ -237,21 +235,24 @@ function decryptWallet(encryptedWallet, userPassword) {
 function getXpub(encryptedWallet, userPassword) {
   // get the xprv from the encrypted wallet and compute the xpub from it
   if ((clearWallet = decryptWallet(encryptedWallet, userPassword)) === "") {
-    console.log("decryptWallet failed");
+    console.error("decryptWallet failed");
     return "";
   }
   
   let wallet_obj = JSON.parse(clearWallet);
 
   if ((xpub_ptr = ccall('xpubFromXprv', 'number', ['string'], [wallet_obj.xprv])) === "") {
-    console.log("xpubFromXprv failed");
+    console.error("xpubFromXprv failed");
     return "";
   };
 
-  let xpub = UTF8ToString(xpub_ptr);
+  if ((xpub = convertToString(xpub_ptr, "xpub")) === "") {
+    return "";
+  }
 
-  if (ccall('wally_free_string', 'number', ['number'], [xpub_ptr]) !== 0) {
-    console.log("xpub wasn't freed");
+  return xpub;
+}
+
     return "";
   }
 
@@ -261,7 +262,7 @@ function getXpub(encryptedWallet, userPassword) {
 function getSeed(encryptedWallet, userPassword) {
   // get the seed from encrypted wallet
   if ((clearWallet = decryptWallet(encryptedWallet, userPassword)) === "") {
-    console.log("decryptWallet failed");
+    console.error("decryptWallet failed");
     return "";
   }
   
@@ -273,7 +274,7 @@ function getSeed(encryptedWallet, userPassword) {
 function getMasterBlindingKey(encryptedWallet, userPassword) {
   // get the master blinding key
   if ((clearWallet = decryptWallet(encryptedWallet, userPassword)) === "") {
-    console.log("decryptWallet failed");
+    console.error("decryptWallet failed");
     return "";
   }
   
@@ -295,8 +296,7 @@ function newConfidentialAddressFromScript(script, encryptedWallet, userPassword)
     return "";
   }
 
-  if ((privateBlindingKey = convertToString(privateBlindingKey_ptr)) === "") {
-    console.error("Failed to convert newTx to string");
+  if ((privateBlindingKey = convertToString(privateBlindingKey_ptr, "privateBlindingKey")) === "") {
     return "";
   }
 
@@ -306,8 +306,7 @@ function newConfidentialAddressFromScript(script, encryptedWallet, userPassword)
     return "";
   }
 
-  if ((address = convertToString(address_ptr)) === "") {
-    console.error("Failed to convert newTx to string");
+  if ((address = convertToString(address_ptr, "address")) === "") {
     return "";
   }
 
@@ -317,8 +316,7 @@ function newConfidentialAddressFromScript(script, encryptedWallet, userPassword)
     return "";
   }
 
-  if ((confidentialAddress = convertToString(confidentialAddress_ptr)) === "") {
-    console.error("Failed to convert newTx to string");
+  if ((confidentialAddress = convertToString(confidentialAddress_ptr, "confidentialAddress")) === "") {
     return "";
   }
 
@@ -336,50 +334,46 @@ function newConfidentialAddressFromXpub(xpub, hdPath, encryptedWallet, userPassw
   let path = parsePath(hdPath);
 
   if ((pubkey_ptr = ccall('getPubkeyFromXpub', 'number', ['string', 'array', 'number'], [xpub, path, path.length])) === 0) {
-    console.log("getPubkeyFromXpub failed");
+    console.error("getPubkeyFromXpub failed");
     return "";
   }
 
-  if ((pubkey = convertToString(pubkey_ptr)) === "") {
-    console.error("Failed to convert pubkey to string");
+  if ((pubkey = convertToString(pubkey_ptr, "pubkey")) === "") {
     return "";
   }
 
   if ((masterBlindingKey = getMasterBlindingKey(encryptedWallet, userPassword)) === "") {
-    console.log("getMasterBlindingKey failed");
+    console.error("getMasterBlindingKey failed");
     return "";
   }
 
   // get the blinding key
   if ((privateBlindingKey_ptr = ccall('getBlindingKeyFromScript', 'number', ['string', 'string'], [pubkey, masterBlindingKey])) === 0) {
-    console.log("getBlindingKeyFromScript failed");
+    console.error("getBlindingKeyFromScript failed");
     return "";
   }
 
-  if ((privateBlindingKey = convertToString(privateBlindingKey_ptr)) === "") {
-    console.error("Failed to convert privateBlindingKey to string");
+  if ((privateBlindingKey = convertToString(privateBlindingKey_ptr, "privateBlindingKey")) === "") {
     return "";
   }
 
   // get the unconfidential address
   if ((address_ptr = ccall('getAddressFromScript', 'number', ['string'], [pubkey])) === 0) {
-    console.log("getAddressFromScript failed");
+    console.error("getAddressFromScript failed");
     return "";
   }
 
-  if ((address = convertToString(address_ptr)) === "") {
-    console.error("Failed to convert address to string");
+  if ((address = convertToString(address_ptr, "address")) === "") {
     return "";
   }
 
   // combine both to get a confidential address
   if ((confidentialAddress_ptr = ccall('getConfidentialAddressFromAddress', 'number', ['string', 'string'], [address, privateBlindingKey])) === 0) {
-    console.log("getConfidentialAddressFromAddress failed");
+    console.error("getConfidentialAddressFromAddress failed");
     return "";
   }
 
-  if ((confidentialAddress = convertToString(confidentialAddress_ptr)) === "") {
-    console.error("Failed to convert confidentialAddress to string");
+  if ((confidentialAddress = convertToString(confidentialAddress_ptr, "confidentialAddress")) === "") {
     return "";
   }
 
@@ -411,8 +405,7 @@ function createProposalTx(previousTx, contractHash, assetAddress, changeAddress,
     return "";
   }
 
-  if ((newTx = convertToString(newTx_ptr)) === "") {
-    console.error("Failed to convert newTx to string");
+  if ((newTx = convertToString(newTx_ptr, "newTx")) === "") {
     return "";
   }
 
